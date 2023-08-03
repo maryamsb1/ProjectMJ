c86c50ea45019b56f2abebf9a7336e76f0f4d7e3
maryamsb1 <msbaskran@gmail.com>
2023-08-03 11:09:08 +0300

    first commit

---
+++ app/src/main/java/com/example/projectmj/athkar_atw1.kt
@@ -0,0 +1,11 @@
+package com.example.projectmj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class athkar_atw1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.athkar_atw1)
+    }
+}
